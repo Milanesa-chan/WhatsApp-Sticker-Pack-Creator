75bd4a06cd2f40525483a35123a73df829b8ff6c
Milanesa-chan <lucagutierrez@hotmail.com>
2018-11-04 05:07:52 -0300

    resizeAllImages() and resizeImage() done.

--- Code/src/milanesa/stickerpackcreator/main/Main.java
+++ Code/src/milanesa/stickerpackcreator/main/Main.java
@@ -1,6 +1,7 @@
 package milanesa.stickerpackcreator.main;
 
 import javax.imageio.ImageIO;
+import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.FileFilter;
@@ -25,7 +26,7 @@ public class Main {
     public static void main(String[] args){
         jarPath = getJarPath();
         System.out.println(jarPath);
-        getImagesFromFolder(jarPath);
+        resizeAllImages(getImagesFromFolder(jarPath));
     }
 
     private static String getJarPath(){
@@ -46,15 +47,20 @@ public class Main {
             File inputDir = new File(mainDirPath.concat("/input"));
 
             if (inputDir.exists() && inputDir.isDirectory()) {
-                if(inputDir.listFiles().length > 0) {
-                    File[] allImageFiles = inputDir.listFiles();
+                File[] allImageFiles = inputDir.listFiles(imageFilenameFilter);
+
+                if (allImageFiles.length >= 3 && allImageFiles.length <=30) {
                     BufferedImage[] images = new BufferedImage[allImageFiles.length];
                     for (int fileIndex = 0; fileIndex < allImageFiles.length; fileIndex++) {
                         images[fileIndex] = ImageIO.read(allImageFiles[fileIndex]);
                         System.out.println("[getImagesFromFolder] Loaded image file: " + allImageFiles[fileIndex].getName());
                     }
                     return images;
-                }else{
+                } else if (allImageFiles.length > 30){
+                    System.out.println("[Error][getImagesFromFolder] Max amount of images is 30. Remove some images!");
+                } else if (allImageFiles.length < 3){
+                    System.out.println("[Error][getImagesFromFolder] Minimum amount of images is 3. Add some images!");
+                } else {
                     System.out.println("[getImagesFromFolder] No valid files found in input folder.");
                     return null;
                 }
@@ -65,5 +71,65 @@ public class Main {
             ex.printStackTrace();
         }
         return null;
+    }
+
+    private static BufferedImage[] resizeAllImages(BufferedImage[] originalImages){
+        BufferedImage[] resizedImages = new BufferedImage[originalImages.length];
+
+        for(int imageIndex=0; imageIndex<resizedImages.length; imageIndex++){
+            resizedImages[imageIndex] = resizeImage(originalImages[imageIndex]);
+            System.out.println("[resizeAllImages] Images resized: "+(imageIndex+1)+"/"+originalImages.length);
+        }
+
+        return resizedImages;
+    }
+
+    private static BufferedImage resizeImage(BufferedImage originalImage){
+        BufferedImage resizedImage = new BufferedImage(512, 512, BufferedImage.TYPE_INT_ARGB);
+
+        Dimension originalDimension = new Dimension(originalImage.getWidth(), originalImage.getHeight());
+
+        Dimension resizedDimension = getNewImageDimensions(
+                originalDimension,
+                new Dimension(512, 512)
+        );
+
+        Point centeredImagePoint = new Point(
+                (512-resizedDimension.width)/2,
+                (512-resizedDimension.height)/2
+        );
+
+        Graphics2D g = resizedImage.createGraphics();
+        g.drawImage(
+                originalImage,
+                centeredImagePoint.x,
+                centeredImagePoint.y,
+                resizedDimension.width,
+                resizedDimension.height,
+                null
+                );
+        g.dispose();
+        return resizedImage;
+    }
+
+    private static Dimension getNewImageDimensions(Dimension imgSize, Dimension boundary){
+        int original_width = imgSize.width;
+        int original_height = imgSize.height;
+        int bound_width = boundary.width;
+        int bound_height = boundary.height;
+        int new_width = original_width;
+        int new_height = original_height;
+
+        if (original_width > bound_width) {
+            new_width = bound_width;
+            new_height = (new_width * original_height) / original_width;
+        }
+
+        if (new_height > bound_height) {
+            new_height = bound_height;
+            new_width = (new_height * original_width) / original_height;
+        }
+
+        return new Dimension(new_width, new_height);
     }
 }
